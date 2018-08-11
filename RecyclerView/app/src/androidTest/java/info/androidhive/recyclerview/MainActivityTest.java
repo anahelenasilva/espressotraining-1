9beb448f9b21097760f32dad09ebec3735290488
Ana Helena <anahelenarp@hotmail.com>
2018-08-11 11:38:29 -0300

    Novos testes

---
+++ RecyclerView/app/src/androidTest/java/info/androidhive/recyclerview/MainActivityTest.java
@@ -0,0 +1,38 @@
+package info.androidhive.recyclerview;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.v7.widget.RecyclerView;
+
+import junit.framework.Assert;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void activityReady() {
+        Espresso.onView(
+                ViewMatchers.withText("Movie")
+        ).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+
+    @Test
+    public void floatButtonisEnabled(){
+        Espresso.onView(
+                ViewMatchers.withId(R.id.imageButton)
+        ).check(ViewAssertions.matches(ViewMatchers.isEnabled()));
+    }
+
+    @Test
+    public  void recyclerViewIsEmpty(){
+        RecyclerView viewById = (RecyclerView)rule.getActivity().findViewById(R.id.recycler_view);
+        Assert.assertTrue(viewById.getAdapter().getItemCount() == 0);
+    }
+}
